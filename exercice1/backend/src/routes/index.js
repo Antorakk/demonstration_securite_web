05e25d80295f25bcadb1b33eb550d232226617c0
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-03-13 19:43:01 +0100

    create link for good form

--- exercice1/backend/src/routes/index.js
+++ exercice1/backend/src/routes/index.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { getUsers, postUser } = require('../controllers');
+const { getUsers, postUser, bestPostUser } = require('../controllers');
 const bodyParser = require('body-parser')
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -8,7 +8,7 @@ const router = Router();
 
 router.get('/users',getUsers);
 router.post('/',urlencodedParser,postUser)
-
+router.post('/good',urlencodedParser,bestPostUser)
 
 module.exports = router;
 
