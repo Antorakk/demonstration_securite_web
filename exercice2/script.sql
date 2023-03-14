0f16b89000d9fc799ca514dd504657825550262f
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-03-14 23:06:34 +0100

    script sql

--- exercice2/script.sql
+++ exercice2/script.sql
@@ -1,10 +1,17 @@
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
-    email VARCHAR(50) NOT NULL,
-    prenom VARCHAR(50) NOT NULL,
-    nom VARCHAR(50) NOT NULL,
-    pwd VARCHAR(50) NOT NULL
+    email VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    pwd VARCHAR(255) NOT NULL
 );
 
 
-INSERT INTO
+INSERT INTO users(email,prenom,nom,pwd) VALUES(
+    'anthony76520.ae@gmail.com','anthony','eluecque','$2b$10$6TLYe/p/eK/CGcXRZ5VVjeuwfJqqjSszX7nTB7RBERPtFITzWzDMq'
+)
+
+
+INSERT INTO users(email,prenom,nom,pwd) VALUES(
+    'antoinebacquet62@gmail.com','antoine','bacquet','$2b$10$eYOZXkWtF2z8lP7VEDDfHO/6pdRzInrHLu5B7w68bQhUCvQaerunu'
+)
