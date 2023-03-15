6c6d5f9d37ae27293486c165bd3c78f47f8d9a5e
Antoinebac435 <antoinebacquet62@gmail.com>
2023-03-15 13:27:16 +0100

    DOUBBBLEEEEE FORMULAIRE

---
+++ exercice2/backend/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+
+
+// app.get('/',(req,res)=> res.send('Bienvenue sur notre API de sécu web'));
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/formulaire.html')
+})
+
+// app.post('/',urlencodedParser,(req,res)=>{
+//     res.sendStatus(200);
+// })
+
+// applique les protections XSS par défaut incluses dans helmet
+// qui nettoieront les entrées utilisateur pour empêcher l'exécution de code malveillant.
+app.use(require('./routes/index.js'));
+app.listen(3000);
+console.log('Server on port 3000');
