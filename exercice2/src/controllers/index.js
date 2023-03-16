a893babfcaf7d933423be7b3d5120840cd466631
Antoinebac435 <antoinebacquet62@gmail.com>
2023-03-16 13:43:07 +0100

    problème

--- exercice2/src/controllers/index.js
+++ exercice2/src/controllers/index.js
@@ -1,12 +1,10 @@
 const { Pool } = require('pg');
-// const xssFilters = require('xss-filters');
-
 
 const pool = new Pool({
-    host:'172.20.10.3',
-    user:'postgres',
+    host:'127.0.0.1',
+    user:'antoinebac',
     password:'bacquet',
-    database:'testsecuwebb',
+    database:'testsecuweb',
     port:'5432'
 })
 
@@ -30,16 +28,6 @@ const postUser = async(req,res) => {
         res.status(200).send(`User bien ajouté`);
       }
     )
-
-    // La bonne version
-    // pool.query(`INSERT INTO utilisateur (nom,prenom,mot_de_passe,rank) 
-    //     VALUES ($1,$2,$3,$4)`,
-    // [nom,prenom,mot_de_passe,rank],
-    // (error, results) => {
-    //     if (error) throw error;
-    //     res.status(200).send(`User bien ajouté`);
-    // }
-    // )   
 }
 
 const bestPostUser = async(req,res) => {
