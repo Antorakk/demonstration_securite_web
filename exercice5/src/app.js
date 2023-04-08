4667966af886d7284b5f6ca0ba845fb10cc0e8a7
Antoinebac435 <antoinebacquet62@gmail.com>
2023-04-08 22:15:57 +0200

    Update app.js and create control and logger
    cannot connect with the database i cant try

--- exercice5/src/app.js
+++ exercice5/src/app.js
@@ -1,9 +1,12 @@
 const {connectTodB} = require('./services/db/connexion')
+const express = require('express');
+const winston = require('winston');
+const DailyRotateFile = require('winston-daily-rotate-file');
+
+const app = express();
+
 app = require("./routes/index.js")
 require('dotenv').config();
-// var xss = require("xss");
-// var html = xss('<script>alert("xss");</script>');
-// console.log(html);
 
 
 const createServer = () => {
@@ -14,3 +17,30 @@ const createServer = () => {
 }
 
 createServer()
+
+const userProfileChangesLogger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new DailyRotateFile({
+      filename: './logs/user-profile-changes-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d'
+    })
+  ]
+});
+
+app.use(express.json());
+
+// définir les routes pour le contrôleur userProfile
+const userProfileRoutes = require('./routes/userProfileRoutes');
+app.use('/user-profile', userProfileRoutes);
+
+module.exports = app;
+
+
---
+++ exercice5/src/controllers/userprofileController.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const userProfileService = require('../services/userProfileService');
+const userProfileChangesLogger = require('../userProfileChangesLogger');
+
+const router = express.Router();
+
+router.put('/:userId', (req, res, next) => {
+  const userId = req.params.userId;
+  const updatedProfile = req.body;
+
+  userProfileService.updateUserProfile(userId, updatedProfile)
+    .then(() => {
+      userProfileChangesLogger.info({
+        message: `Le profil de l'utilisateur ${userId} a été modifié`,
+        userId: userId,
+        updatedProfile: updatedProfile
+      });
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
