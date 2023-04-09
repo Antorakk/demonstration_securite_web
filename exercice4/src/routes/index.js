498500873f149e4d3919036a2b3e924c7bdbcf1a
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-04-09 14:05:38 +0200

    update index.js

--- exercice4/src/routes/index.js
+++ exercice4/src/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const users = require('./users');
 const app = express();
+const mongoSanitize = require('express-mongo-sanitize');
 
 app.engine('pug', require('pug').__express)
 app.set("views",process.cwd()+"/src/views");
 app.set("view engine","pug");
 
+app.use(mongoSanitize())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -12,6 +15,7 @@ app.get('/',(req,res) => {
     res.render('index');
 })
 
+app.use(users)
 
 module.exports = app;
 
