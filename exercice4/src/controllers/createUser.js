478b0699f5e825a72cad35d5f3d7d5a03c275f8d
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-04-09 15:19:19 +0200

    remove useless ""

--- exercice4/src/controllers/createUser.js
+++ exercice4/src/controllers/createUser.js
@@ -5,18 +5,18 @@ const sanitize = require("mongo-sanitize");
 const { Validator } = require("jsonschema");
 const schemaUser = require('../models/user')
 
+
 const users = config.tables.users
 const createUser = async (req,res) => {
 
     try {
-
         const cleanUser = sanitize(req.query)
         const user = {
-            "nom":cleanUser.nom,
-            "prenom":cleanUser.prenom,
-            "mail":cleanUser.mail,
-            "password": await hashPwd(cleanUser.password),
-            "otp" : ""
+            nom:cleanUser.nom,
+            prenom:cleanUser.prenom,
+            mail:cleanUser.mail,
+            password: await hashPwd(cleanUser.password),
+            otp : ""
         };
         let validator = new Validator();
         if (validator.validate(user,schemaUser).valid){
