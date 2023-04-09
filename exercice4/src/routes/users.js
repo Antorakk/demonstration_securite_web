3c80aa6f1cbfe20146f70bbf0a99fefec9ace00d
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-04-09 14:04:49 +0200

    prevent nosql injection

--- exercice4/src/routes/users.js
+++ exercice4/src/routes/users.js
@@ -2,13 +2,15 @@ const { Router } = require('express');
 const router = Router();
 const express = require('express');
 const createUser = require('../controllers/createUser');
+const loginUser = require('../controllers/loginUser');
 // const { verificationIdentity , verificationOtp } = require('../controllers/verificationIdentity');
 // const { transporter } = require('../models/transporter');
-
+const mongoSanitize = require('express-mongo-sanitize');
+router.use(mongoSanitize())
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
 
 router.post('/create-user',createUser);
-
+router.post('/login-user',loginUser)
 
 module.exports = router;
