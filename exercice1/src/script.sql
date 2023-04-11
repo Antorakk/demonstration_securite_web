31dff8add192c5815cd8ac23c9c8806561071af9
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-04-11 19:19:09 +0200

    fix

--- exercice1/src/script.sql
+++ exercice1/src/script.sql
@@ -6,21 +6,9 @@ CREATE TABLE utilisateur (
   rank VARCHAR(255) NOT NULL
 );
 
-INSERT INTO utilisateur(nom,prenom,numero_tel,mot_de_passe,rank) VALUES
-    ('Dupont', 'Jean', '06 12 34 56 78', 'motdepasse1' , 1);--"test",2);
-
-`INSERT INTO utilisateur(nom,prenom,numero_tel,mot_de_passe,rank) VALUES ('Dupont', 'Jean', '06 12 34 56 78', 'motdepasse1' , 1);-- ,2);)`
-
-
-INSERT INTO utilisateur (nom, prenom, numero_tel, mot_de_passe , rank)
+INSERT INTO utilisateur (nom, prenom, mot_de_passe , rank)
 VALUES 
-  ('Dupont', 'Jean', '06 12 34 56 78', 'motdepasse1' , 1),
-  ('Martin', 'Marie', '07 23 45 67 89', 'motdepasse2' , 2);
+  ('Dupont', 'Jean', 'motdepasse1' , 1),
+  ('Martin', 'Marie', 'motdepasse2' , 2);
 
-
-
-CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    prenom VARCHAR(255) NOT NULL
-)
 
